0afd8aad4f3f2fddfca48d091eee4cc416265421
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-14 06:44:30 +0800

    feat: Unactivated page, getSSR resuable function

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import enforceAuthenticated from "../components/enforceAuthenticated";
 import Layout from "../components/layout/Layout";
-import Waiting from "../components/Waiting";
 
-export default function Home({ user, acct_type, data }) {
+export default function Home({ acct_type, data }) {
   console.log(data);
 
   return (
     <>
-      {acct_type === "vendor" && !data?.activated ? <Waiting /> : <></>}
       <Layout title="Home" acct_type={acct_type}>
         {acct_type}
       </Layout>
@@ -15,27 +13,4 @@ export default function Home({ user, acct_type, data }) {
   );
 }
 
-export const getServerSideProps = async (ctx) => {
-  const supabase = createServerSupabaseClient(ctx);
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session)
-    return { redirect: { destination: "/login", permanent: false } };
-
-  const { data: user_data } = await supabase.from("users").select("*");
-  const { data: vendor_data } = await supabase.from("vendors").select("*");
-  const acct_type = user_data.length
-    ? "customer"
-    : vendor_data.length
-    ? "vendor"
-    : 0;
-  const data = user_data.length
-    ? user_data[0]
-    : vendor_data.length
-    ? vendor_data[0]
-    : {};
-  return { props: { acct_type, data } };
-};
+export const getServerSideProps = enforceAuthenticated();
---
+++ pages/unactivated.js
@@ -0,0 +1,40 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Waiting() {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const handleSignOut = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+
+    if (!error) router.push("/login");
+  };
+
+  return (
+    <>
+      <div className="bg-white fixed top-0 left-0 right-0 bottom-0 z-50 flex flex-col gap-6 items-center justify-center pt-48">
+        <p className="text-3xl font-bold z-50">Registration Completed</p>
+        <p className="text-xl font-semibold text-center mx-12 z-50">
+          {
+            "Please wait for your account to be activated. This may take a couple of days."
+          }
+        </p>
+        <button
+          type="button"
+          onClick={handleSignOut}
+          className="bg-teal text-cream rounded-full px-5 py-2 hover:opacity-75"
+        >
+          sign out
+        </button>
+        <Image
+          className="absolute top-16"
+          src="/graphics/waiting.png"
+          alt=""
+          width={500}
+          height={500}
+        />
+      </div>
+    </>
+  );
+}
