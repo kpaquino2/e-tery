e822e22f91c4ccb29a9b8d3fc2fd3f303e6b89b3
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-15 15:01:47 +0800

    chore: Remove console.log

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -6,7 +6,6 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Header({ acct_type }) {
-  console.log(acct_type);
   const supabaseClient = useSupabaseClient();
   const user = useUser();
   const router = useRouter();
