01a0d8f8278c5acd3e5a0d5d7af3e88ef2a4c02b
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-16 13:49:48 +0800

    feat: Item availability

--- components/vendor/Item.js
+++ components/vendor/Item.js
@@ -1,22 +1,45 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Image from "next/image";
+import { useState } from "react";
+import { MdDoNotDisturbAlt } from "react-icons/md";
 
 export default function Item({ data }) {
-  console.log(data);
+  const supabaseClient = useSupabaseClient();
+  const [itemAvailable, setItemAvailable] = useState(data.available);
+
+  const updateAvailability = async () => {
+    const { error } = await supabaseClient
+      .from("items")
+      .update({ available: !itemAvailable })
+      .eq("id", data.id);
+    if (!error) setItemAvailable(!itemAvailable);
+  };
+
   return (
-    <div>
-      <Image
-        className="rounded-t-3xl"
-        src={`items/${data.id}`}
-        alt={data.name}
-        width={200}
-        height={200}
-      />
+    <div className="drop-shadow-lg">
+      <div className="relative">
+        <Image
+          className={"rounded-t-3xl " + (itemAvailable ? "" : "opacity-50")}
+          src={`items/${data.id}`}
+          alt={data.name}
+          width={200}
+          height={200}
+        />
+        {!itemAvailable ? (
+          <MdDoNotDisturbAlt className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-maroon text-8xl shadow-lg" />
+        ) : (
+          <></>
+        )}
+      </div>
       <div className="flex flex-col bg-teal text-cream text-md rounded-b-3xl px-4 py-2 w-full">
         <p className="truncate font-semibold">{data.name}</p>
         <p className="truncate">Php {data.base_price.toFixed(2)}</p>
-        <p className="text-xs pt-2">
-          SET AS {data.available ? "UNAVAILABLE" : "AVAILABLE"}
-        </p>
+        <button
+          className="underline decoration-solid text-xs pt-2"
+          onClick={updateAvailability}
+        >
+          SET AS {itemAvailable ? "UNAVAILABLE" : "AVAILABLE"}
+        </button>
       </div>
     </div>
   );
