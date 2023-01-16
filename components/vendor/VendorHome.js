1a7dd22115900a10cacb8fa00c22b6d17429db03
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-16 09:29:15 +0800

    feat: Update vendor home with store details

---
+++ components/vendor/VendorHome.js
@@ -0,0 +1,17 @@
+import Banner from "./Banner";
+
+export default function VendorHome({ data }) {
+  return (
+    <div className="flex flex-col items-center">
+      <Banner />
+      <div className="font-bold text-4xl">{data.name}</div>
+      <div className="flex flex-col">
+        {data.categories.map((value, index) => (
+          <div key={index} className="text-2xl font-semibold">
+            {value.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
