3415edf42faa735262d30416b612b4c45533178e
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-17 09:31:44 +0800

    feat: Show empty state in customer home

--- components/customer/CustomerHome.js
+++ components/customer/CustomerHome.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AdCarousel from "./AdCarousel";
 import Store from "./Store";
 
@@ -7,9 +8,24 @@ export default function CustomerHome({ stores }) {
       <AdCarousel />
       <div className="font-bold text-4xl mb-2">Stores</div>
       <div className="flex flex-col gap-4">
-        {stores.map((value, index) => (
-          <Store details={value} key={index} />
-        ))}
+        {stores.length ? (
+          stores.map((value, index) => <Store details={value} key={index} />)
+        ) : (
+          <div className="place-self-center">
+            <Image
+              src="graphics/notready.png"
+              alt="NO STORES"
+              width={600}
+              height={600}
+            />
+            <div className="text-2xl font-bold text-center">
+              {"stores aren't ready yet"}
+            </div>
+            <div className="text-lg font-semibold text-center">
+              come back later!
+            </div>
+          </div>
+        )}
       </div>
     </dic>
   );
