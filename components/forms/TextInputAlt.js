3e16dc92e2fdcb4a0bff9cffcbc586106c0fe6e0
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-17 15:04:16 +0800

    feat: New alternative text input

---
+++ components/forms/TextInputAlt.js
@@ -0,0 +1,27 @@
+export default function TextInputAlt({
+  label,
+  placeholder,
+  type = "text",
+  error,
+  register,
+  name,
+}) {
+  return (
+    <>
+      <div className="flex gap-2 items-center text-light text-2xl">
+        <label>{label}:</label>
+        <input
+          type={type}
+          className="border-x-0 border-t-0 border-b-2 border-light bg-dark focus:ring-0 text-xl p-1 focus:border-inherit"
+          placeholder={placeholder}
+          {...register(name)}
+        />
+      </div>
+      {error ? (
+        <div className="text-red-500 font-semibold ml-2 text-sm">{error}</div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
