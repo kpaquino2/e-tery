ef6cb3144930d3061bf151e13c47ed64f0b22b99
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-18 15:43:25 +0800

    feat: Change look based on if there is image

--- components/vendor/Item.js
+++ components/vendor/Item.js
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { useState } from "react";
 import { MdDoNotDisturbAlt } from "react-icons/md";
 
-export default function Item({ data }) {
+export default function Item({ vendor_id, data }) {
   const supabaseClient = useSupabaseClient();
   const [itemAvailable, setItemAvailable] = useState(data.available);
+  const [isImageLoaded, setisImageLoaded] = useState(true);
 
   const updateAvailability = async () => {
     const { error } = await supabaseClient
@@ -16,22 +17,34 @@ export default function Item({ data }) {
   };
 
   return (
-    <div className="drop-shadow-lg">
+    <div
+      className={
+        "w-full py-1 drop-shadow-lg break-inside-avoid " +
+        (itemAvailable ? "" : "opacity-50")
+      }
+    >
       <div className="relative">
         <Image
-          className={"rounded-t-2xl " + (itemAvailable ? "" : "opacity-50")}
-          src={`items/${data.id}`}
-          alt={data.name}
+          className={"rounded-t-2xl bg-teal " + (isImageLoaded ? "" : "hidden")}
+          src={`items/${vendor_id}/${data.id}`}
+          alt=""
           width={400}
           height={400}
+          onError={() => setisImageLoaded(false)}
         />
-        {!itemAvailable ? (
-          <MdDoNotDisturbAlt className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-maroon text-8xl shadow-lg" />
+
+        {!itemAvailable && isImageLoaded ? (
+          <MdDoNotDisturbAlt className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-maroon text-8xl" />
         ) : (
           <></>
         )}
       </div>
-      <div className="flex flex-col bg-teal text-cream text-md rounded-b-2xl px-4 py-2 w-full">
+      <div
+        className={
+          (isImageLoaded ? "rounded-b-2xl" : "rounded-2xl") +
+          " flex flex-col bg-teal text-cream text-md px-4 py-2 w-full"
+        }
+      >
         <p className="truncate font-semibold">{data.name}</p>
         <p className="truncate">Php {data.base_price.toFixed(2)}</p>
         <button
