4bb4d3fca5fb487dd67dd2c242ee233c7e503467
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-19 05:49:28 +0800

    feat: List categories and items in store pages

---
+++ components/customer/StoreItem.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { useState } from "react";
+
+export default function StoreItem({ vendor_id, data }) {
+  const [isImageLoaded, setisImageLoaded] = useState(true);
+  return (
+    <div
+      className={
+        "w-full py-1 break-inside-avoid " + (data.available ? "" : "opacity-50")
+      }
+    >
+      <Image
+        className={"rounded-t-2xl bg-cream " + (isImageLoaded ? "" : "hidden")}
+        src={`items/${vendor_id}/${data.id}`}
+        alt=""
+        width={400}
+        height={400}
+        onError={() => setisImageLoaded(false)}
+      />
+      <div
+        className={
+          (isImageLoaded ? "rounded-b-2xl" : "rounded-2xl") +
+          " flex flex-col bg-cream text-dark text-md px-4 py-2 w-full break-words"
+        }
+      >
+        <span className="font-semibold">{data.name}</span>
+        <span className="text-sm">{data.description}</span>
+        <span className="">Php {data.base_price.toFixed(2)}</span>
+      </div>
+    </div>
+  );
+}
--- pages/store/[id].js
+++ pages/store/[id].js
@@ -1,5 +1,6 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/router";
+import StoreItem from "../../components/customer/StoreItem";
 import Layout from "../../components/layout/Layout";
 import Banner from "../../components/vendor/Banner";
 
@@ -10,6 +11,24 @@ export default function StorePage({ store }) {
         <div className="flex flex-col items-center">
           <Banner id={store.id} />
           <div className="font-bold text-4xl">{store.name}</div>
+          <div className="flex flex-col px-2 pb-12 w-full">
+            {store.categories.length ? (
+              store.categories.map((category, index) => (
+                <div key={index}>
+                  <div className="text-2xl font-semibold text-center">
+                    {category.name}
+                  </div>
+                  <div className="columns-2 gap-2">
+                    {category.items.map((item, index) => (
+                      <StoreItem key={index} vendor_id={store.id} data={item} />
+                    ))}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </Layout>
     </>
@@ -22,7 +41,7 @@ export const getServerSideProps = async (ctx) => {
   const { data } = await supabase
     .from("vendors")
     .select(
-      "id, name, categories (id, name, items (id, name, base_price, available))"
+      "id, name, open, categories (id, name, desc, items (id, name, description, base_price, available))"
     )
     .eq("id", ctx.params.id);
   console.log(data);
