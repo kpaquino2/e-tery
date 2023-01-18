6d9fe66ce59404580307603072b29dd3f6fb0146
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-19 06:17:08 +0800

    feat: Category carousel

--- pages/store/[id].js
+++ pages/store/[id].js
@@ -3,19 +3,45 @@ import { useRouter } from "next/router";
 import StoreItem from "../../components/customer/StoreItem";
 import Layout from "../../components/layout/Layout";
 import Banner from "../../components/vendor/Banner";
+import { useKeenSlider } from "keen-slider/react";
+import Link from "next/link";
 
 export default function StorePage({ store }) {
+  const [sliderRef] = useKeenSlider({
+    mode: "free-snap",
+    slides: {
+      origin: "center",
+      perView: 4,
+      spacing: 20,
+    },
+  });
+
   return (
     <>
       <Layout title={store.name}>
         <div className="flex flex-col items-center">
           <Banner id={store.id} />
-          <div className="font-bold text-4xl">{store.name}</div>
-          <div className="flex flex-col px-2 pb-12 w-full">
+          <div className="font-bold text-4xl m-auto">{store.name}</div>
+          <div ref={sliderRef} className="my-2 keen-slider">
+            {store.categories.map((category, index) => (
+              <Link
+                key={index}
+                href={`#${category.id}`}
+                className="text-center keen-slider__slide"
+              >
+                {category.name}
+              </Link>
+            ))}
+          </div>
+          <div className="bg-cream h-1 w-11/12 rounded-full" />
+          <div className="flex flex-col pb-12 w-full">
             {store.categories.length ? (
               store.categories.map((category, index) => (
                 <div key={index}>
-                  <div className="text-2xl font-semibold text-center">
+                  <div
+                    className="text-2xl my-2 font-semibold text-center"
+                    id={category.id}
+                  >
                     {category.name}
                   </div>
                   <div className="columns-2 gap-2">
