360c6abfb5871a52097add533cc72b060fde910c
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-19 11:13:03 +0800

    chore: Banner accepts url instead of store id

--- components/vendor/Banner.js
+++ components/vendor/Banner.js
@@ -1,16 +1,25 @@
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Banner({ id }) {
+export default function Banner({ url }) {
+  const [isLoaded, setIsLoaded] = useState(true);
+
   return (
     <>
-      <div className="relative top-0 left-0 right-0 w-full flex justify-center ">
+      <div
+        className={
+          "relative top-0 left-0 right-0 w-full flex justify-center " +
+          (isLoaded ? "" : "hidden")
+        }
+      >
         <span className="absolute w-full h-1/2 bg-maroon" />
         <Image
-          className="clip-banner drop-shadow-2xl"
-          src={`banners/${id}`}
+          className="bg-cream clip-banner drop-shadow-2xl"
+          src={url}
           alt="banner"
           width={600}
           height={300}
+          onError={() => setIsLoaded(false)}
         />
       </div>
     </>
