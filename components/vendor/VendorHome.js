97f3cbc35e19e7618804f59698d27028f499d297
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-21 05:55:39 +0800

    feat: Change add item menu to page

--- components/vendor/VendorHome.js
+++ components/vendor/VendorHome.js
@@ -5,19 +5,11 @@ import { FaPlus } from "react-icons/fa";
 import AddItemMenu from "./AddItemMenu";
 import Banner from "./Banner";
 import Item from "./Item";
+import Link from "next/link";
 
 export default function VendorHome({ id, data }) {
-  const [isAddItemMenuOpen, setIsAddItemMenuOpen] = useState(false);
-  const [categoryToAdd, setCategoryToAdd] = useState("");
-
   return (
     <>
-      <AddItemMenu
-        isOpen={isAddItemMenuOpen}
-        setIsOpen={setIsAddItemMenuOpen}
-        vendor_id={id}
-        category_id={categoryToAdd}
-      />
       <div className="flex flex-col items-center">
         <Banner id={id} />
         <div className="font-bold text-4xl">{data.name}</div>
@@ -32,19 +24,15 @@ export default function VendorHome({ id, data }) {
                     <Item key={index} vendor_id={id} data={item} />
                   ))}
                   <div className="py-1">
-                    <button
-                      type="button"
+                    <Link
+                      href={`add-item/${category.id}`}
                       className="flex flex-row py-4 gap-2 w-full items-center justify-center rounded-2xl bg-teal"
-                      onClick={() => {
-                        setCategoryToAdd(category.id);
-                        setIsAddItemMenuOpen(true);
-                      }}
                     >
                       <FaPlus className="text-2xl text-maroon" />
                       <div className="text-xl font-bold text-cream">
                         add item
                       </div>
-                    </button>
+                    </Link>
                   </div>
                 </div>
               </div>
---
+++ pages/add-item/[category_id].js
@@ -0,0 +1,217 @@
+import { HiUpload } from "react-icons/hi";
+
+import * as yup from "yup";
+import { useFieldArray, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import TextInputAlt from "../../components/forms/TextInputAlt";
+import Upload from "../../components/upload/Upload";
+import OptionsFields from "../../components/vendor/OptionsFields";
+import Layout from "../../components/layout/Layout";
+import { FaChevronLeft } from "react-icons/fa";
+import { useRouter } from "next/router";
+
+const schema = yup.object({
+  name: yup.string().required("name is required"),
+  price: yup
+    .number()
+    .required("base price is required")
+    .typeError("please put a valid price"),
+  desc: yup.string(),
+  variations: yup.array(
+    yup.object({
+      name: yup.string().required("variation name is required"),
+      min: yup
+        .number()
+        .typeError("please put a valid minimum")
+        .required("please put a minimum"),
+      max: yup
+        .number()
+        .typeError("please put a valid maximum")
+        .required("please put a maximum"),
+      options: yup.array(
+        yup.object({
+          name: yup.string().required("option name is required"),
+          addtl_price: yup
+            .number()
+            .typeError("please put a valid price")
+            .required("please put a valid price"),
+        })
+      ),
+    })
+  ),
+});
+
+export default function AddItemPage({ vendor_id, category_id }) {
+  const supabaseClient = useSupabaseClient();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    control,
+  } = useForm({ resolver: yupResolver(schema) });
+  const { fields, append, remove } = useFieldArray({
+    name: "variations",
+    control,
+  });
+  const router = useRouter();
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    // setLoading(true);
+    // const {
+    //   data: [responseData],
+    //   error,
+    // } = await supabaseClient
+    //   .from("items")
+    //   .insert([
+    //     {
+    //       name: data.name,
+    //       base_price: data.price,
+    //       description: data.desc,
+    //       vendor_id: vendor_id,
+    //       category_id: category_id,
+    //     },
+    //   ])
+    //   .select();
+
+    // if (!error) {
+    //   if (image) {
+    //     supabaseClient.storage
+    //       .from("items")
+    //       .upload(vendor_id + "/" + responseData.id, image);
+    //   }
+    //   router.replace(router.asPath).then(() => {
+    //     setIsOpen(false);
+    //     reset();
+    //     setImage("");
+    //     setLoading(false);
+    //   });
+    // }
+  };
+
+  const addVariation = () => {
+    append({ name: "", min: "", max: "" });
+  };
+
+  return (
+    <>
+      <Layout title="New Item">
+        <div className="relative flex flex-col items-center bg-dark grow pb-12">
+          <button
+            type="button"
+            className="absolute top-6 left-4 rounded-full p-1"
+            onClick={() => router.back()}
+            disabled={loading}
+          >
+            <FaChevronLeft className="text-3xl text-cream" />
+          </button>
+          <p className="text-cream text-4xl font-bold mb-4 mt-12">Details</p>
+          <form
+            className="flex flex-col items-center gap-2 text-cream w-3/4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <TextInputAlt
+              label="Name"
+              register={register}
+              error={errors.name?.message}
+              name="name"
+            />
+            <TextInputAlt
+              label="Description"
+              register={register}
+              name="desc"
+              placeholder="Optional"
+            />
+            <TextInputAlt
+              label="Price"
+              register={register}
+              error={errors.price?.message}
+              name="price"
+            />
+
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id}>
+                  <TextInputAlt
+                    label={`Variation ${index + 1}`}
+                    register={register}
+                    error={errors.variations?.[index]?.name?.message}
+                    name={`variations.${index}.name`}
+                  />
+                  <TextInputAlt
+                    label="Minimum selection"
+                    register={register}
+                    error={errors.variations?.[index]?.min?.message}
+                    name={`variations.${index}.min`}
+                  />
+                  <TextInputAlt
+                    label="Maximum selection"
+                    register={register}
+                    error={errors.variations?.[index]?.max?.message}
+                    name={`variations.${index}.max`}
+                  />
+                  <OptionsFields
+                    index={index}
+                    register={register}
+                    control={control}
+                    errors={errors}
+                  />
+                </div>
+              );
+            })}
+            <button
+              type="button"
+              onClick={addVariation}
+              className="underline text-2xl"
+            >
+              + add variation
+            </button>
+            <p className="text-cream text-4xl font-bold my-4">Insert Image:</p>
+            <Upload
+              finalImage={image}
+              setFinalImage={setImage}
+              aspect={1}
+              height={400}
+              width={400}
+              register={register}
+              name="image"
+              loading={loading}
+            >
+              <div className="bg-teal rounded-2xl w-full p-4">
+                <HiUpload className="m-auto text-5xl text-maroon" />
+              </div>
+            </Upload>
+            <button
+              type="submit"
+              className={
+                "rounded-full bg-teal text-white font-bold text-lg w-min px-8 py-1 mt-8 " +
+                (loading ? "opacity-75 cursor-wait" : "")
+              }
+              disabled={loading}
+            >
+              SUBMIT
+            </button>
+          </form>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+export const getServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  return {
+    props: { vendor_id: session?.user.id, category_id: ctx.params.category_id },
+  };
+};
