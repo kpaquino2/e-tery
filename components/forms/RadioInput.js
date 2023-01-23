5eecf4cc7a1e48d1ff75d55554cb37ed758d4a2a
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-23 09:09:42 +0800

    chore: Better style format radioinput

--- components/forms/RadioInput.js
+++ components/forms/RadioInput.js
@@ -5,13 +5,7 @@ export default function RadioInput({ id, value, size, register, name }) {
         id={id}
         type="radio"
         value={value}
-        className={
-          "w-" +
-          size +
-          " h-" +
-          size +
-          " text-teal bg-gray-100 border-maroon focus:ring-teal"
-        }
+        className={`w-${size} h-${size} text-teal bg-gray-100 border-maroon focus:ring-teal`}
         {...register(name)}
       />
     </>
