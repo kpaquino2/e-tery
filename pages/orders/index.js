7d45d6eb5dd332cb086ff3bade2000b9a2235ee3
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-25 16:58:07 +0800

    feat: Orders page update

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -3,12 +3,16 @@ import { motion } from "framer-motion";
 import moment from "moment/moment";
 import Link from "next/link";
 import { useState } from "react";
-import { FaClock } from "react-icons/fa";
+import { FaClock, FaRegClock } from "react-icons/fa";
 import Layout from "../../components/layout/Layout";
 
 export default function OrdersPage({ orders }) {
   const [tab, setTab] = useState(0);
-  const statusColor = { pending: "bg-gray-400 text-gray-900" };
+  const statusColor = {
+    pending: "bg-gray-400 text-gray-900",
+    accepted: "bg-blue-400 text-blue-900",
+    declined: "bg-red-400 text-red-900",
+  };
 
   return (
     <>
@@ -41,77 +45,49 @@ export default function OrdersPage({ orders }) {
           </div>
         </div>
         <div className="flex flex-col m-2 gap-2">
-          {tab === 0
-            ? orders?.map(
-                (order, index) =>
-                  order.status !== "delivered" && (
-                    <Link
-                      href={`/orders/${order.id}`}
-                      key={index}
-                      className="p-2 bg-teal rounded-lg text-cream grid grid-cols-[2fr_1fr]"
-                    >
-                      <span className="flex items-center gap-1 font-semibold text-lg">
-                        <FaClock className="text-maroon" />
-                        {moment(order.time, "HH:mm:ss").format("hh:mm A")}
-                      </span>
-                      <span className="text-lg row-span-3 col-start-2 font-semibold place-self-center">
-                        ₱ {order.total.toFixed(2)}
-                      </span>
-                      <span>
-                        {order.payment_option === "cod"
-                          ? "Cash on Delivery"
-                          : "Over the counter"}
-                      </span>
-
-                      <span>
-                        {order.delivery_option === "delivery"
-                          ? "Room Delivery"
-                          : "Pickup"}
-                      </span>
-                      <span
-                        className={
-                          "w-min px-3 rounded-full col-span-full place-self-center " +
-                          statusColor[order.status]
-                        }
-                      >
-                        {order.status.toUpperCase()}
-                      </span>
-                    </Link>
-                  )
-              )
-            : orders?.map((order, index) => (
-                <div
+          {orders?.map(
+            (order, index) =>
+              (tab === 1 ||
+                [
+                  "in-transit",
+                  "ready",
+                  "prepared",
+                  "accepted",
+                  "pending",
+                ].includes(order.status)) && (
+                <Link
+                  href={`/orders/${order.id}`}
                   key={index}
-                  className="p-2 bg-teal rounded-lg text-cream grid grid-cols-[2fr_1fr]"
+                  className="p-2 bg-cream rounded-lg text-teal grid grid-cols-[2fr_1fr]"
                 >
-                  <span className="flex items-center gap-1 font-semibold text-lg">
-                    <FaClock className="text-maroon" />
-                    {moment(order.time, "HH:mm:ss").format("hh:mm A")}
-                  </span>
-                  <span className="text-lg row-span-3 col-start-2 font-semibold place-self-center">
-                    ₱ {order.total.toFixed(2)}
-                  </span>
                   <span>
                     {order.payment_option === "cod"
                       ? "Cash on Delivery"
                       : "Over the counter"}
                   </span>
-
+                  <span
+                    className={
+                      statusColor[order.status] +
+                      " flex row-span-2 m-1 justify-center items-center font-semibold rounded-lg"
+                    }
+                  >
+                    {order.status.toUpperCase()}
+                  </span>
                   <span>
                     {order.delivery_option === "delivery"
                       ? "Room Delivery"
                       : "Pickup"}
                   </span>
-                  <span
-                    className={
-                      "w-min px-3 rounded-full col-span-full place-self-center " +
-                      statusColor[order.status]
-                    }
-                  >
-                    {order.status.toUpperCase()}
+                  <span className="flex items-center gap-1">
+                    <FaRegClock className="text-maroon" />
+                    {moment(order.time, "HH:mm:ss").format("hh:mm A")}
                   </span>
-                </div>
-              ))}
+                  <span className="text-lg font-semibold text-center">
+                    ₱ {order.total.toFixed(2)}
+                  </span>
+                </Link>
+              )
+          )}
         </div>
       </Layout>
     </>
@@ -125,7 +101,8 @@ export const getServerSideProps = async (ctx) => {
     .from("orders")
     .select(
       "id, total, payment_option, delivery_option, time, status, room_id, order_rating"
-    );
+    )
+    .order("time", { ascending: false });
 
   return { props: { orders: data } };
 };
