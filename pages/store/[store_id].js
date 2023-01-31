aaa0f086dfd4baaf50746ca92a8d1e26dc7fb5a1
Kyle Aquino <kpaquino2@up.edu.ph>
2023-01-31 10:03:07 +0800

    feat: Show empty state for stores

--- pages/store/[store_id].js
+++ pages/store/[store_id].js
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { useRef } from "react";
 import { FaChevronLeft } from "react-icons/fa";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 export default function StorePage({ store }) {
   const router = useRouter();
@@ -56,7 +57,8 @@ export default function StorePage({ store }) {
           </div>
           <div className="bg-cream h-1 w-11/12 rounded-full" />
           <div className="flex flex-col pb-12 px-2 w-full">
-            {store.categories.length ? (
+            {store.categories.length &&
+            store.categories.map((c) => c.items.length > 0).includes(true) ? (
               store.categories.map((category, index) => {
                 if (!category.items.length) return;
                 return (
@@ -80,7 +82,20 @@ export default function StorePage({ store }) {
                 );
               })
             ) : (
-              <></>
+              <>
+                <Image
+                  src="graphics/empty.png"
+                  alt="empty store"
+                  width={600}
+                  height={500}
+                />
+                <div className="text-2xl font-bold text-center">
+                  the store seems empty
+                </div>
+                <div className="text-lg font-semibold text-center">
+                  try again in another time
+                </div>
+              </>
             )}
           </div>
         </div>
