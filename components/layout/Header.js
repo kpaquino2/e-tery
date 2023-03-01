db9e3eb3a3d4fa109b4a255e7a4605af963f4cf9
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-02 01:03:44 +0800

    refactor: useSession in Header instead of useUser

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,33 +5,35 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import VendorNav from "../vendor/VendorNav";
-import { useUser } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
 import CustomerNav from "../customer/CustomerNav";
+import { useSession } from "@supabase/auth-helpers-react";
 
 export default function Header() {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
-  const user = useUser();
+  const session = useSession();
   const [acctType, setAcctType] = useState("");
 
   useEffect(() => {
-    if (user)
+    if (session && session.user)
       setAcctType(
-        user.email?.endsWith("@g.batstate-u.edu.ph") ? "customer" : "vendor"
+        session.user.email?.endsWith("@g.batstate-u.edu.ph")
+          ? "customer"
+          : "vendor"
       );
-  }, [user]);
+  }, [session]);
 
   return (
     <>
-      <header className="fixed top-0 max-w-[450px] w-screen left-1/2 -translate-x-1/2 m-auto bg-maroon z-30">
-        <div className="flex justify-between items-center px-3 h-20">
+      <header className="fixed top-0 left-1/2 z-30 m-auto w-screen max-w-[450px] -translate-x-1/2 bg-maroon">
+        <div className="flex h-20 items-center justify-between px-3">
           {acctType === "customer" ? (
-            <CustomerNav customer_id={user?.id} />
+            <CustomerNav customer_id={session?.user.id} />
           ) : acctType === "vendor" ? (
-            <VendorNav vendor_id={user?.id} />
+            <VendorNav vendor_id={session?.user.id} />
           ) : (
-            <div className="grid place-items-center w-full">
+            <div className="grid w-full place-items-center">
               <Image
                 src="graphics/logo-light.png"
                 alt=""
