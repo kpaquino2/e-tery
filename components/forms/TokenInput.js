b6e27188d0a8c20e55e1bad0cc8e90f53b441dfd
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-02 20:15:22 +0800

    feat: Max tries and resend confirm token

--- components/forms/TokenInput.js
+++ components/forms/TokenInput.js
@@ -1,26 +1,39 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BiMailSend } from "react-icons/bi";
 
 export default function TokenInput({ acct_type, data, setLoading, banner }) {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
-
-  const [token, setToken] = useState(Array(6));
+  const [timer, setTimer] = useState(0);
+  const [tries, setTries] = useState(0);
+  const disable = tries > 2 ? true : false;
+  const length = 6;
   const [confirmError, setConfirmError] = useState("");
-  const ref1 = useRef(null);
-  const ref2 = useRef(null);
-  const ref3 = useRef(null);
-  const ref4 = useRef(null);
-  const ref5 = useRef(null);
-  const ref6 = useRef(null);
+
+  const inputs = useRef([]);
+  const [token, setToken] = useState("");
+  console.log(token.length);
+
+  useEffect(() => {
+    if (timer > 0) {
+      const time = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(time);
+      };
+    }
+  }, [timer]);
 
   const confirmEmail = async () => {
+    setConfirmError("");
     setLoading(true);
     const { data: cdata, error } = await supabaseClient.auth.verifyOtp({
       email: data.email,
-      token: token.join(""),
+      token: inputs.current.map((c) => c.value).join(""),
       type: "signup",
     });
     if (!error) {
@@ -51,112 +64,95 @@ export default function TokenInput({ acct_type, data, setLoading, banner }) {
     }
     setLoading(false);
     setConfirmError(error.message);
+    setTries((p) => p + 1);
+  };
+
+  const resend = async () => {
+    await supabaseClient.auth.signUp({
+      email: data.email,
+      password: data.password,
+    });
+    setTimer(120);
   };
 
   return (
-    <div className="flex flex-1 flex-col items-center gap-2 py-24">
+    <div className="flex w-full flex-1 flex-col items-center gap-2 px-4 pt-24 pb-12">
       <BiMailSend className="rounded-full bg-teal pt-2 pr-2 text-8xl text-cream" />
-      <div className="text-3xl font-bold text-dark">
+      <div className="text-center text-3xl font-bold text-dark">
         Enter Confirmation Token
       </div>
-      <div className="mb-8 font-semibold text-burgundy">
+      <div className="mb-8  text-center font-semibold text-burgundy">
         Check your email for the token!
       </div>
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-1">
-          <input
-            type="text"
-            inputMode="numeric"
-            className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
-            ref={ref1}
-            onChange={(e) => {
-              e.target.value = e.target.value.trim();
-              if (e.target.value.length > 1)
-                e.target.value = e.target.value.slice(-1);
-              if (e.target.value) ref2.current.focus();
-              setToken((prev) => [...prev.fill(e.target.value, 0, 1)]);
-            }}
-          />
-          <input
-            type="text"
-            inputMode="numeric"
-            className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
-            ref={ref2}
-            onChange={(e) => {
-              e.target.value = e.target.value.trim();
-              if (e.target.value.length > 1)
-                e.target.value = e.target.value.slice(-1);
-              if (e.target.value) ref3.current.focus();
-              setToken((prev) => [...prev.fill(e.target.value, 1, 2)]);
-            }}
-          />
-          <input
-            type="text"
-            inputMode="numeric"
-            className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
-            ref={ref3}
-            onChange={(e) => {
-              e.target.value = e.target.value.trim();
-              if (e.target.value.length > 1)
-                e.target.value = e.target.value.slice(-1);
-              if (e.target.value) ref4.current.focus();
-              setToken((prev) => [...prev.fill(e.target.value, 2, 3)]);
-            }}
-          />
-          <input
-            type="text"
-            inputMode="numeric"
-            className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
-            ref={ref4}
-            onChange={(e) => {
-              e.target.value = e.target.value.trim();
-              if (e.target.value.length > 1)
-                e.target.value = e.target.value.slice(-1);
-              if (e.target.value) ref5.current.focus();
-              setToken((prev) => [...prev.fill(e.target.value, 3, 4)]);
-            }}
-          />
-          <input
-            type="text"
-            inputMode="numeric"
-            className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
-            ref={ref5}
-            onChange={(e) => {
-              e.target.value = e.target.value.trim();
-              if (e.target.value.length > 1)
-                e.target.value = e.target.value.slice(-1);
-              if (e.target.value) ref6.current.focus();
-              setToken((prev) => [...prev.fill(e.target.value, 4, 5)]);
-            }}
-          />
-          <input
-            type="text"
-            inputMode="numeric"
-            className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
-            ref={ref6}
-            onChange={(e) => {
-              e.target.value = e.target.value.trim();
-              if (e.target.value.length > 1)
-                e.target.value = e.target.value.slice(-1);
-              if (e.target.value) ref6.current.blur();
-              setToken((prev) => [...prev.fill(e.target.value, 5, 6)]);
-            }}
-          />
+      {disable ? (
+        <div className="flex flex-col text-center text-maroon">
+          <p className="text-xl font-bold">Max attempts exceeded</p>
+          <p className="font-semibold">try again later</p>
         </div>
-        {confirmError && (
-          <div className="font-semibold text-red-500">
-            {confirmError.toLowerCase()}
+      ) : (
+        <div className="flex flex-col gap-3">
+          <div className="flex gap-1">
+            {[...Array(length)].map((_, i) => (
+              <input
+                key={i}
+                type="text"
+                inputMode="numeric"
+                className="w-12 rounded-xl border-none bg-cream text-center text-lg focus:ring-maroon"
+                ref={(el) => (inputs.current[i] = el)}
+                onFocus={(e) => {
+                  e.target.value = "";
+                  setToken(inputs.current.map((c) => c.value).join(""));
+                }}
+                onChange={(e) => {
+                  e.target.value = e.target.value.trim();
+                  let x = 0;
+                  for (; x + i + 1 < length; x++) {
+                    if (!e.target.value[x + 1]) break;
+                    inputs.current[i + x + 1].value = e.target.value[x + 1];
+                  }
+                  e.target.value = e.target.value[0] || "";
+                  if (e.target.value)
+                    inputs.current[x + i + 1]
+                      ? inputs.current[x + i + 1].focus()
+                      : inputs.current[i].blur();
+                  setToken(inputs.current.map((c) => c.value).join(""));
+                }}
+              />
+            ))}
           </div>
-        )}
-        <button
-          type="button"
-          onClick={confirmEmail}
-          className="w-full rounded-full bg-maroon px-10 py-1 text-lg font-bold leading-tight text-cream hover:brightness-110 disabled:brightness-75"
-          disabled={token.join("").length !== 6}
-        >
-          confirm
-        </button>
-      </div>
+          {confirmError && (
+            <div className="text-sm font-semibold text-red-500">
+              {confirmError.toLowerCase()}
+            </div>
+          )}
+          <button
+            type="button"
+            onClick={confirmEmail}
+            className="w-full rounded-full bg-maroon px-10 py-1 text-lg font-bold leading-tight text-cream hover:brightness-110 disabled:brightness-75"
+            disabled={token.length < 6}
+          >
+            confirm
+          </button>
+          <p className="text-center text-dark">
+            {timer > 0 ? (
+              <>
+                {"resend again in "}
+                <p className="inline font-bold">
+                  {Math.floor(timer / 60)}:
+                  {(timer % 60).toString().padStart(2, "0")}
+                </p>
+              </>
+            ) : (
+              <>
+                {"didn't receive the token? "}
+                <button className="inline font-bold" onClick={resend}>
+                  resend
+                </button>
+              </>
+            )}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
